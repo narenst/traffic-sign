d17f87ad16c04ccffc1b902bc74461246314b22c
Naren Thiagarajan <narenst@gmail.com>
2018-01-04 14:58:12 -0800

    Add train step

---
+++ stats.md
@@ -0,0 +1,5 @@
+## Results
+
+batch_size=32, lr=0.01, decay=1e-6, momentum=0.9
+
+![Image](https://cl.ly/3d221l1n3q02)
---
+++ train.py
@@ -0,0 +1,137 @@
+import numpy as np
+from skimage import io, color, exposure, transform
+from sklearn.cross_validation import train_test_split
+import os
+import glob
+import h5py
+from time import time
+
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential, model_from_json
+from keras.layers.core import Dense, Dropout, Activation, Flatten
+from keras.layers.convolutional import Conv2D
+from keras.layers.pooling import MaxPooling2D
+from keras.callbacks import TensorBoard
+
+from keras.optimizers import SGD
+from keras.utils import np_utils
+from keras.callbacks import LearningRateScheduler, ModelCheckpoint
+from keras import backend as K
+K.set_image_data_format('channels_first')
+
+NUM_CLASSES = 43
+IMG_SIZE = 48
+
+
+def preprocess_img(img):
+    # Histogram normalization in y
+    hsv = color.rgb2hsv(img)
+    hsv[:,:,2] = exposure.equalize_hist(hsv[:,:,2])
+    img = color.hsv2rgb(hsv)
+
+    # central scrop
+    min_side = min(img.shape[:-1])
+    centre = img.shape[0]//2, img.shape[1]//2
+    img = img[centre[0]-min_side//2:centre[0]+min_side//2,
+              centre[1]-min_side//2:centre[1]+min_side//2,
+              :]
+
+    # rescale to standard size
+    img = transform.resize(img, (IMG_SIZE, IMG_SIZE))
+
+    # roll color axis to axis 0
+    img = np.rollaxis(img,-1)
+
+    return img
+
+
+def get_class(img_path):
+    return int(img_path.split('/')[-2])
+
+
+try:
+    with h5py.File('/input/X.h5') as hf:
+        X, Y = hf['imgs'][:], hf['labels'][:]
+    print("Loaded images from X.h5")
+
+except (IOError,OSError, KeyError):  
+    print("Error in reading X.h5. Processing all images...")
+    root_dir = 'GTSRB/Final_Training/Images/'
+    imgs = []
+    labels = []
+
+    all_img_paths = glob.glob(os.path.join(root_dir, '*/*.ppm'))
+    np.random.shuffle(all_img_paths)
+    for img_path in all_img_paths:
+        try:
+            img = preprocess_img(io.imread(img_path))
+            label = get_class(img_path)
+            imgs.append(img)
+            labels.append(label)
+
+            if len(imgs)%1000 == 0: print("Processed {}/{}".format(len(imgs), len(all_img_paths)))
+        except (IOError, OSError):
+            print('missed', img_path)
+            pass
+
+    X = np.array(imgs, dtype='float32')
+    Y = np.eye(NUM_CLASSES, dtype='uint8')[labels]
+
+    with h5py.File('X.h5','w') as hf:
+        hf.create_dataset('imgs', data=X)
+        hf.create_dataset('labels', data=Y)
+
+
+def cnn_model():
+    model = Sequential()
+
+    model.add(Conv2D(32, (3, 3), padding='same',
+                     input_shape=(3, IMG_SIZE, IMG_SIZE),
+                     activation='relu'))
+    model.add(Conv2D(32, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.2))
+
+    model.add(Conv2D(64, (3, 3), padding='same',
+                     activation='relu'))
+    model.add(Conv2D(64, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.2))
+
+    model.add(Conv2D(128, (3, 3), padding='same',
+                     activation='relu'))
+    model.add(Conv2D(128, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.2))
+
+    model.add(Flatten())
+    model.add(Dense(512, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(NUM_CLASSES, activation='softmax'))
+    return model
+
+model = cnn_model()
+# let's train the model using SGD + momentum (how original).
+lr = 0.01
+sgd = SGD(lr=lr, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(loss='categorical_crossentropy',
+              optimizer=sgd,
+              metrics=['accuracy'])
+tensorboard = TensorBoard(log_dir="/output/logs/{}".format(time()), histogram_freq=0, batch_size=32, write_graph=True, write_grads=False, write_images=False, embeddings_freq=0, embeddings_layer_names=None, embeddings_metadata=None)
+
+def lr_schedule(epoch):
+    return lr*(0.1**int(epoch/10))
+
+
+batch_size = 32
+nb_epoch = 30
+
+model.fit(X, Y,
+          batch_size=batch_size,
+          epochs=nb_epoch,
+          validation_split=0.2,
+          shuffle=True,
+          verbose=1,
+          callbacks=[tensorboard,
+                     LearningRateScheduler(lr_schedule),
+                     ModelCheckpoint('/output/traffic_sign_model.h5', save_best_only=True)])
